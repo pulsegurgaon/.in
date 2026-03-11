5d6c1b49b701b9218ad3d93c577eb49d72abea72
rt01-boop <mailbox.rt01@gmail.com>
2026-03-11 16:02:50 +0530

    Create frontend/script.js

---
+++ frontend/script.js
@@ -0,0 +1,38 @@
+let lang="en"
+
+function toggleLang(){
+lang=lang==="en"?"hi":"en"
+loadNews()
+}
+
+async function loadNews(){
+
+const res=await fetch("https://YOUR_RENDER_URL/news")
+
+const data=await res.json()
+
+const row=document.getElementById("newsrow")
+
+row.innerHTML=""
+
+data.forEach(a=>{
+
+const card=document.createElement("div")
+card.className="card"
+
+let title=lang==="en"?a.title_en:a.title_hi
+let summary=lang==="en"?a.summary_en:a.summary_hi
+
+card.innerHTML=`
+<img src="${a.image}">
+<h3>${title}</h3>
+<p>${summary}</p>
+`
+
+row.appendChild(card)
+
+})
+
+}
+
+loadNews()
