42af5246a490ff05a080e318293a08a2ca1c3d9f
pulsegurgaon <mailbox.rt01@gmail.com>
2026-03-14 23:48:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-let lang = "en"
+let lang="en"
 
 function toggleLang(){
-lang = lang === "en" ? "hi" : "en"
+lang = lang==="en" ? "hi" : "en"
 loadNews()
 }
 
 async function loadNews(){
 
 const row = document.getElementById("newsrow")
-row.innerHTML = ""
+
+if(!row) return
+
+row.innerHTML=""
 
 const res = await fetch("articles.json")
+
 const data = await res.json()
 
-data.forEach(article => {
+data.forEach(article=>{
 
-const title = lang === "en" ? article.title_en : article.title_hi
-const summary = lang === "en" ? article.summary_en : article.summary_hi
+const title = lang==="en" ? article.title_en : article.title_hi
+const summary = lang==="en" ? article.summary_en : article.summary_hi
 
 const card = document.createElement("div")
-card.className = "card"
+card.className="card"
 
 card.innerHTML = `
 <a href="frontend/article.html?id=${article.id}">
 <img src="${article.image}">
+
 <h3>${title}</h3>
 <p>${summary}</p>
 </a>
-`
-
-row.appendChild(card)
+`row.appendChild(card)
 
 })
 
